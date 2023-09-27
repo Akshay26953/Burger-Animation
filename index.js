77827166327ac789c8f7ec9ab4cea0d88b55b6e5
Akshay26953 <amkharat2695@gmail.com>
2023-09-27 21:18:46 +0530

    added new button for burger animation

---
+++ index.js
@@ -0,0 +1,37 @@
+const h1 = document.getElementById("h1");
+const h2 = document.getElementById("h2");
+const para = document.getElementById("para");
+const images = document.querySelectorAll(".img");
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+const redBg = document.querySelector(".redBg");
+const burger_top = document.querySelector(".burger_top");
+function load() {
+  h1.style.left = "9.5%";
+  h2.style.left = "10%";
+  para.style.left = "10%";
+  images.forEach((img) => {
+    img.style.right = "10%";
+  });
+  btn.style.transform = "scale(1)";
+}
+
+btn.addEventListener("click", () => {
+  h1.style.left = "-40%";
+  h2.style.left = "-40%";
+  para.style.left = "-50%";
+  redBg.style.left = "-15%";
+  redBg.style.transform = "rotate(0)";
+  images.forEach((img) => {
+    img.style.right = "50%";
+    img.style.transform = "translateX(50%)";
+  });
+});
+
+btn1.addEventListener("click", () => {
+  images.forEach((img) => {
+    img.style.transitionProperty = "top";
+    img.style.transitionDuration= "1s";
+  });
+  burger_top.style.top = "20%";
+});
